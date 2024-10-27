d3d353604923f984d9680d5f24abe855a163f0a8
nbdbkv <nurbakyt.abdibekov@gmail.com>
2024-10-28 02:08:20 +0600

    add post's image admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from posts.models import Country, Tag, Post
+from posts.models import Country, Tag, PostImage, Post
 
 
 @admin.register(Country)
@@ -16,7 +16,13 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 
+class PostImageInline(admin.TabularInline):
+    model = PostImage
+    extra = 0
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ('topic', 'user', 'country', 'is_shown')
     list_display_links = list_display
+    inlines = (PostImageInline,)
