0daf71cf492ccfa81cafeaffb04c4ed5b49a8273
nbdbkv <nurbakyt.abdibekov@gmail.com>
2024-10-28 02:43:58 +0600

    add post's comment model

--- posts/models.py
+++ posts/models.py
@@ -69,3 +69,15 @@ class PostImage(models.Model):
 
     def __str__(self):
         return self.image.name
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', verbose_name='Пост')
+    text = models.TextField(verbose_name='Текст')
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+
+    def __str__(self):
+        return f'{self.post.topic} - {self.text[:10]}'
