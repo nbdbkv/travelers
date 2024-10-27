f0cab19500145baf69f1cf79f433ecce4671cd9e
nbdbkv <nurbakyt.abdibekov@gmail.com>
2024-10-27 21:23:35 +0600

    add country admin

--- posts/admin.py
+++ posts/admin.py
@@ -1,2 +1,11 @@
 from django.contrib import admin
 
+from posts.models import Country
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'alpha2Code', 'alpha3Code', 'capital', 'region')
+    list_display_links = list_display
+    search_fields = list_display
+    ordering = ('name',)
