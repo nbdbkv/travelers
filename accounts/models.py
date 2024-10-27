88a194b784fab90cc6cc1d6ca97cdd5483ee52d7
nbdbkv <nurbakyt.abdibekov@gmail.com>
2024-10-27 14:25:36 +0600

    add CustomUser model

--- accounts/models.py
+++ accounts/models.py
@@ -1,2 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from accounts.managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True, verbose_name='Электронная почта')
+    otp = models.CharField(max_length=6, null=True, blank=True, verbose_name='Код')
+    can_post = models.BooleanField(default=True, verbose_name='Создать пост')
+    has_access = models.BooleanField(default=True, verbose_name='Доступ')
+    is_active = models.BooleanField(default=False, verbose_name='Активный')
+    verify_date = models.DateTimeField(null=True, blank=True, verbose_name='Дата верификации')
+
+    username = None
+
+    objects = CustomUserManager()
+
+    REQUIRED_FIELDS = []
+    USERNAME_FIELD = 'email'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
--- core/settings.py
+++ core/settings.py
@@ -131,6 +131,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
