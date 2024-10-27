433a2bc47ae0a5607082aa8b59799a8df54e2873
nbdbkv <nurbakyt.abdibekov@gmail.com>
2024-10-27 14:28:22 +0600

    add CustomUser admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from accounts.models import CustomUser
 
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'can_post', 'has_access', 'is_active', 'verify_date')
+    list_display_links = list_display
+    search_fields = ('email',)
